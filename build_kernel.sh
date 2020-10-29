de28d8ec214258c38e735fb34b880934e2acae56
Jimbo77 <chupyalcohol@gmail.com>
2020-10-29 13:37:16 -0600

    updates to build Script to use test defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,9 +12,9 @@ export CROSS_COMPILE=$(pwd)/toolchains/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-l
 export ANDROID_MAJOR_VERSION=q
 export CLANG_TRIPLE=aarch64-linux-gnu-
 
-make exynos7885-gta3xlwifi_defconfig
+make test-gta3xlwifi_defconfig
 make -j$(nproc)
 
-cp arch/$ARCH/boot/Image.gz $(pwd)/out/Image.gz
+cp arch/$ARCH/boot/Image $(pwd)/out/Image
 
 #make mrproper
