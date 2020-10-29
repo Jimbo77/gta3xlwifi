8d6813736b2387c079493a076b1164f2d913d05b
Jimbo77 <chupyalcohol@gmail.com>
2020-10-29 13:19:07 -0600

    update to the build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=toolchains/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
+export CLANG_TRIPLE=aarch64-linux-gnu-
 
-make exynos7885-gta3xlwifi_defconfig
+make test-gta3xlwifi_defconfig
 make -j64
