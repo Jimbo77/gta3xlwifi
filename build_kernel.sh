0c2f2bca575bf23cb2dabcfb7852b775fc054484
Jimbo77 <chupyalcohol@gmail.com>
2020-10-29 12:22:43 -0600

    updates to build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
+
+rm -rf $(pwd)/out/Image.gz
 
 export ARCH=arm64
-export CROSS_COMPILE=toolchains/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+make mrproper
+
+mkdir out
+
+export CROSS_COMPILE=$(pwd)/toolchains/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 export CLANG_TRIPLE=aarch64-linux-gnu-
 
 make exynos7885-gta3xlwifi_defconfig
-make -j64
+make -j$(nproc)
+
+cp arch/$ARCH/boot/Image.gz $(pwd)/out/Image.gz
+
+#make mrproper
